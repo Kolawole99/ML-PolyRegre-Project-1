4e2a56b46373bb98864ccb9e041b445ad6cf2a80
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-16 09:34:53 +0100

    Project scaffolding, importing packages and modules

---
+++ PR.py
@@ -0,0 +1,12 @@
+#==================================IMPORTING NEEDED PACKAGES============================
+import matplotlib.pyplot as plt
+import pandas as pd
+import pylab as pl
+import numpy as np
+#%matplotlib inline #for jupyter notes
+
+
+
+#==========================================DATA========================================
+
+

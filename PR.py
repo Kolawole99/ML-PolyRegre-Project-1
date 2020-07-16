a67d43e10b2bc1f04e0622823e88054fc2e8d070
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-16 10:00:44 +0100

    Polynomial regression evaluation using R2-score

--- PR.py
+++ PR.py
@@ -5,6 +5,7 @@ import pylab as pl
 import numpy as np
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn import linear_model
+from sklearn.metrics import r2_score
 #%matplotlib inline #for jupyter notes
 
 
@@ -73,4 +74,12 @@ plt.show()
 
 
 #=========================================EVALUATION======================================
+test_x_poly = poly.fit_transform(test_x)
+test_y_ = clf.predict(test_x_poly)
+
+print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
+print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
+print("R2-score: %.2f" % r2_score(test_y_ , test_y) )
+
+
 

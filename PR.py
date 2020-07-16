19e9ffcdf4ec9e7cc794ac884fefb52cee5e1b8a
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-16 09:37:13 +0100

    Reading the data into the project

--- PR.py
+++ PR.py
@@ -8,5 +8,18 @@ import numpy as np
 
 
 #==========================================DATA========================================
+# MODELYEAR e.g. 2014
+# MAKE e.g. Acura
+# MODEL e.g. ILX
+# VEHICLE CLASS e.g. SUV
+# ENGINE SIZE e.g. 4.7
+# CYLINDERS e.g 6
+# TRANSMISSION e.g. A6
+# FUEL CONSUMPTION in CITY(L/100 km) e.g. 9.9
+# FUEL CONSUMPTION in HWY (L/100 km) e.g. 8.9
+# FUEL CONSUMPTION COMB (L/100 km) e.g. 9.2
+# CO2 EMISSIONS (g/km) e.g. 182 --> low --> 0
 
-
+df = pd.read_csv("FuelConsumption.csv")
+# take a look at the dataset
+print(df.head())

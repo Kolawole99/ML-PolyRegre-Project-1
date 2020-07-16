0a3f1d32753d66e881597038f71fca03272b7086
Kolawole Ojo <philliphopeojo@gmail.com>
2020-07-16 09:57:16 +0100

    Polynomial regression to train model

--- PR.py
+++ PR.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pylab as pl
 import numpy as np
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import linear_model
 #%matplotlib inline #for jupyter notes
 
 
@@ -43,8 +45,32 @@ test = cdf[~msk]
 
 
 #==================================POLYNOMIAL REGRESSION====================================
+train_x = np.asanyarray(train[['ENGINESIZE']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+
+test_x = np.asanyarray(test[['ENGINESIZE']])
+test_y = np.asanyarray(test[['CO2EMISSIONS']])
+
+poly = PolynomialFeatures(degree=2)
+train_x_poly = poly.fit_transform(train_x)
+print(train_x_poly)
+
+# fit_transform takes our x values, and output a list of our data raised from power of 0 to power of 2 (since we set the degree of our polynomial to 2) so we can use LinearRegression() function to solve it
+clf = linear_model.LinearRegression()
+train_y_ = clf.fit(train_x_poly, train_y)
+# The coefficients
+print ('Coefficients: ', clf.coef_)
+print ('Intercept: ',clf.intercept_)
+
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+XX = np.arange(0.0, 10.0, 0.1)
+yy = clf.intercept_[0]+ clf.coef_[0][1]*XX+ clf.coef_[0][2]*np.power(XX, 2)
+plt.plot(XX, yy, '-r' )
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
 
 
 
-
+#=========================================EVALUATION======================================
 
